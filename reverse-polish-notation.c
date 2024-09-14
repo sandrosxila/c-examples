9c4ec941ee4b8cb8d3ca19a5f86d0d2331033aa4
sandrosxila <sandrosxila@yahoo.com>
2024-09-14 22:48:53 +0400

    reverse polish notation

---
+++ reverse-polish-notation.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int calculate(char operator, int a, int b){
+  switch (operator)
+  {
+  case '+':
+    return a + b;
+  case '-':
+    return a - b;
+  case '*':
+    return a * b;
+  case '/':
+    return a / b;
+  default:
+    return 0;
+  }
+}
+
+int main() {
+
+  char expr[] = "50 3 4 + 5 * -";
+  int stack[100], top = 0;
+
+  for(char* token = strtok(expr, " "); token != NULL; token = strtok(NULL, " ")){
+    char op = token[0];
+    if (op == '+' || op == '-' || op == '*' || op == '/'){
+      int result = calculate(op, stack[--top], stack[--top]);
+      stack[top++] = result;
+    }
+    else {
+      stack[top++] = atoi(token);
+    }
+  }
+
+  if(top != 1){
+    printf("incorrect expression!\n");
+    return 0;
+  }
+
+  int res = stack[0];
+
+  printf("%d\n", res);
+
+  return 0;
+}
